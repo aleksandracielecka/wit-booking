29a628e00404cab2461e8b9464380b066378d3c8
gosia-zontek <131551826+gosia-zontek@users.noreply.github.com>
2023-09-19 21:35:57 +0200

    implementation NewEventDtoValidator + klasa testowa

--- src/main/java/pl/sdacademy/booking/validator/NewEventDtoValidator.java
+++ src/main/java/pl/sdacademy/booking/validator/NewEventDtoValidator.java
@@ -9,30 +9,37 @@ import java.util.List;
 public class NewEventDtoValidator {
     public static List<String> validate(NewEventDto newEventDto) {
         List<String> result = new ArrayList<>();
-        //date is null or empty
+
         if (newEventDto.getFromTime() == null) {
             result.add("From is null");
         }
         if (newEventDto.getToTime() == null) {
             result.add("To is null");
         }
-        //warunki
 
-        //from and to are opposite
-        //date from the past
-        //8-16 work time
-        // how long is event
-        //
-        //item name is null
-        if (newEventDto.getFromTime() != null && newEventDto.getToTime() != null) {
+        //date in the future
+        //date from 8 to 16
+        //how long is event
+        if(newEventDto.getFromTime() != null && newEventDto.getToTime() != null) {
             Duration duration = Duration.between(newEventDto.getFromTime(), newEventDto.getToTime());
             if (duration.isNegative()) {
                 result.add("To is before from");
             }
             if (duration.toMinutes() > 30) {
-                result.add("to id before event");
+                result.add("Too long event");
             }
         }
+
+
+
+
+
+
+
+
+
+        //item name is null
         return result;
     }
+
 }
